263212b1eae025402b7f010851e212f85cfcce64
Tamiris Rezende Garcia Costa <tamirisrgarcia@gmail.com>
2022-01-24 13:16:46 -0300

    implementação dos Cards da Dashboard via EndPoint (branch teste)

--- src/app/modelosInterface/dashboard.ts
+++ src/app/modelosInterface/dashboard.ts
@@ -1,6 +1,9 @@
 export interface Dashboard {
-  title: string;
+  titulo: string;
+  subtitulo: string;
   img: string;
+  conteudo: string;
+  link: [{ref: string, texto: string}];
   cols: number;
   rows: number;
 }
